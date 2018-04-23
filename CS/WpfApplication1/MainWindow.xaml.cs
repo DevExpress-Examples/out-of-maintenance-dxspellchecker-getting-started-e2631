371d02bf9bc66cdcd3e29a71c30c2152ec48d889
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/WpfApplication1/MainWindow.xaml.cs
@@ -0,0 +1,33 @@
+﻿#region #code
+using System.Windows;
+using System.Globalization;
+using DevExpress.Xpf.SpellChecker;
+
+namespace WpfApplication1
+{
+    public partial class MainWindow : Window
+    {
+        SpellChecker checker;
+
+        public MainWindow()
+        {
+            checker = new SpellChecker();
+            InitializeComponent();
+            checker.Culture = new CultureInfo("pl-PL");
+            SpellChecker = checker;
+        }
+
+        public SpellChecker SpellChecker
+        {
+            get { return checker; }
+            private set { checker = value; }
+        }
+
+        private void button1_Click(object sender, RoutedEventArgs e)
+        {
+            // Start checking the text within a text edit control.
+            checker.Check(textEdit1);
+        }
+    }
+}
+#endregion #code
---
+++ Readme.md
@@ -0,0 +1,8 @@
+# DXSpellChecker - Getting Started
+
+
+<p>This tutorial demonstrates how to use the <a href="http://documentation.devexpress.com/#WPF/clsDevExpressXpfSpellCheckerSpellCheckertopic"><u>SpellChecker</u></a> component to create a simple WPF application with the spell-checker functionality. It can be useful for you in solving this task if you are a first-time user of <strong>DXSpellChecker for WPF</strong>.</p>
+
+<br/>
+
+
