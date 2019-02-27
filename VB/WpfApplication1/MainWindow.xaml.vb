2ecd40a9d760d2ba4a94cf4d08df6a1e57cb12e6
DevExpressExampleBot <fakeEmail>
2019-02-27 12:50:41 +0300

    Source auto update [skip ci]

--- VB/WpfApplication1/MainWindow.xaml.vb
+++ VB/WpfApplication1/MainWindow.xaml.vb
@@ -4,32 +4,32 @@ Imports System.Globalization
 Imports DevExpress.Xpf.SpellChecker
 
 Namespace WpfApplication1
-	Partial Public Class MainWindow
-		Inherits Window
+    Partial Public Class MainWindow
+        Inherits Window
 
-		Private checker As SpellChecker
+        Private checker As SpellChecker
 
-		Public Sub New()
-			DataContext = Me
-			checker = New SpellChecker()
-			InitializeComponent()
-			checker.Culture = New CultureInfo("pl-PL")
-			SpellChecker = checker
-		End Sub
+        Public Sub New()
+            DataContext = Me
+            checker = New SpellChecker()
+            InitializeComponent()
+            checker.Culture = New CultureInfo("pl-PL")
+            SpellChecker = checker
+        End Sub
 
-		Public Property SpellChecker() As SpellChecker
-			Get
-				Return checker
-			End Get
-			Private Set(ByVal value As SpellChecker)
-				checker = value
-			End Set
-		End Property
+        Public Property SpellChecker() As SpellChecker
+            Get
+                Return checker
+            End Get
+            Private Set(ByVal value As SpellChecker)
+                checker = value
+            End Set
+        End Property
 
-		Private Sub button1_Click(ByVal sender As Object, ByVal e As RoutedEventArgs)
-			' Start checking the text within a text edit control.
-			checker.Check(textEdit1)
-		End Sub
-	End Class
+        Private Sub button1_Click(ByVal sender As Object, ByVal e As RoutedEventArgs)
+            ' Start checking the text within a text edit control.
+            checker.Check(textEdit1)
+        End Sub
+    End Class
 End Namespace
 #End Region ' #code
